907248c9831d20062d2e0ef66083831db29aad4e
skyb3rt <t.kristoffersen@gmail.com>
2025-02-02 11:44:32 +0100

    notebook

--- main.py
+++ main.py
@@ -56,3 +56,51 @@ def regn_ut_gjennomsnittlig_samtaletid(varigheter:np) -> float:
     return sum_varighet/len(varighet)
 
 print(f"Gjennomsnittlig samtaletid: {regn_ut_gjennomsnittlig_samtaletid(varighet)}")
+
+# Oppgave e.  antall henvendelser tidsrommene 08-10, 10-12, 12-14 og 14-16 for uke 24
+
+def antall_henvendelser_per_bolk(klokkeslett:np)-> dict:
+    '''Funksjon som returnerer antall henvendelser per tidsblokk'''
+    antall= {"08-10":0, "10-12":0, "12-14":0, "14-16":0}
+    for tid in klokkeslett:
+        time=int(tid.split(":")[0])
+        match time:
+            case 8|9:
+                antall["08-10"]+=1
+            case 10|11:
+                antall["10-12"]+=1
+            case 12|13:
+                antall["12-14"]+=1
+            case 14|15|16:
+                antall["14-16"]+=1
+    return antall
+
+antall_pr_blokk=antall_henvendelser_per_bolk(kl_slett)
+'''kakedigram'''
+#plt.close()
+plt.pie(antall_pr_blokk.values(), labels=antall_pr_blokk.keys(), autopct='%1.1f%%')
+plt.title("Antall henvendelser per tidsblokk")
+#plt.show()
+
+# Oppgave f. Supportavdelingens NP
+def supportavdelingens_np(tilbakemeldinger:np) -> float:
+    '''Funksjon som regner ut supportavdelingens NP'''
+    fornoyd_score={"postiv":0, "noytral":0, "negativ":0}
+    for tilbakemelding in tilbakemeldinger:
+        if tilbakemelding: # Utelukker None / Nan verdiene
+            match tilbakemelding:
+                case _ as status if 1<=status<=6:
+                    fornoyd_score["negativ"]+=1
+                case _ as status if 7<=status<=8:
+                    fornoyd_score["noytral"]+=1
+                case _ as status if 9<=status<=10:
+                    fornoyd_score["postiv"]+=1
+    antall_tilbakemeldinger = sum(fornoyd_score.values())
+    print(fornoyd_score)
+    print(antall_tilbakemeldinger)
+    print(fornoyd_score["postiv"]/antall_tilbakemeldinger*100)
+    print(fornoyd_score["negativ"]/antall_tilbakemeldinger*100)
+    return ((fornoyd_score["postiv"]/antall_tilbakemeldinger)-
+            (fornoyd_score["negativ"]/antall_tilbakemeldinger))*100
+
+print(f"Supportavdelingens NP: {supportavdelingens_np(score):.2f}%")
